29e6df5fceba4cd5528429d1a1f04f3900a8f083
ismaelmormor <ismaelmormor@gmail.com>
2023-05-10 13:13:50 +0200

    Commit partes finalizadas del A

---
+++ TablaEnteros/src/main/TablaEnterosTest.java
@@ -0,0 +1,88 @@
+/**
+ * 
+ */
+package main;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author ismael
+ *
+ */
+class TablaEnterosTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+		System.out.println("Test finalizado");
+	}
+
+	/**
+	 * Test method for {@link main.TablaEnteros#TablaEnteros(java.lang.Integer[])}.
+	 */
+	@Test
+	void testTablaEnteros() {
+		Integer[] numeros = {1,2,3};
+		TablaEnteros tabla1 = new TablaEnteros(numeros);
+	}
+
+	/**
+	 * Test method for {@link main.TablaEnteros#sumaTabla()}.
+	 */
+	@Test
+	void testSumaTabla() {
+		Integer[] numeros = {1,2,3};
+		TablaEnteros tabla1 = new TablaEnteros(numeros);
+		tabla1.sumaTabla();
+	}
+
+	/**
+	 * Test method for {@link main.TablaEnteros#mayorTabla()}.
+	 */
+	@Test
+	void testMayorTabla() {
+		Integer[] numeros = {1,2,3};
+		TablaEnteros tabla1 = new TablaEnteros(numeros);
+		tabla1.mayorTabla();
+	}
+
+	/**
+	 * Test method for {@link main.TablaEnteros#posicionTabla(int)}.
+	 */
+	@Test
+	void testPosicionTabla() {
+		Integer[] numeros = {1,2,3};
+		TablaEnteros tabla1 = new TablaEnteros(numeros);
+		tabla1.posicionTabla(3);
+	}
+
+}
--- TablaEnteros/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author ismael
- *
- */
-module TablaEnteros {
-}
